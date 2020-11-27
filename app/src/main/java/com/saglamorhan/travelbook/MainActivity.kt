c65a8b3ca60156791855cb616578d533f66c92f8
saglamorhan61 <saglamorhan.61@gmail.com>
2020-11-27 17:26:41 +0300

    Initial commit, menu ekleme

---
+++ app/src/main/java/com/saglamorhan/travelbook/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.saglamorhan.travelbook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_place,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_place_option){
+
+            val intent = Intent(applicationContext,MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
